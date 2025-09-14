d33b384985bfc4836aee33f9f5ed4a183a01d276
Brendan Apple <brendancapple@gmail.com>
2025-09-14 19:05:33 -0400

    Added the hand, rotating around the player

--- src/player_script.gd
+++ src/player_script.gd
@@ -1,29 +1,61 @@
-extends CharacterBody2D
+extends Node2D
 
 # Member Variables
-@onready var _animated_sprite = $PlayerSprite
+@onready var _player = $Player
+@onready var _player_sprite = $Player/PlayerSprite
+@onready var _player_hitbox = $Player/PlayerHitbox
+@onready var _hand = $Hand
+@onready var _hand_sprite = $Hand/HandSprite
+@onready var _hand_hitbox = $Hand/HandHitbox
 
 @export var speed = 400
 @export var friction = 2.0
-var acceleration = Vector2(0.0, 0.0)
+
+var player_acceleration = Vector2(0.0, 0.0)
+
+var hand_acceleration = Vector2(0.0, 0.0)
+var hand_state = 0 # 0: Idle  1: Extending  2: Latched  3: Pull  4: Push
 
 # Movement Handling
 func process_movement(delta: float):
 	var input_direction = Input.get_vector("Left", "Right", "Up", "Down")
 	
-	acceleration = input_direction * speed
-	acceleration -= velocity * friction;
-	velocity += acceleration * delta
+	player_acceleration = input_direction * speed
+	player_acceleration -= _player.velocity * friction;
+	_player.velocity += player_acceleration * delta
 	
 	if (input_direction == Vector2(0,0)):
-		_animated_sprite.play("idle")
+		_player_sprite.play("idle")
 	else:
-		_animated_sprite.play("walk")
+		_player_sprite.play("walk")
+		
+# Hand Handling
+func position_hand(aim: Vector2):
+	aim = aim.normalized()
+	print(aim)
+	print(_player_hitbox.shape)
+	match hand_state:
+		0: # IDLE
+			print(_player_hitbox.shape.radius)
+			_hand.position = _player.position + (_player_hitbox.shape.radius + 100) * aim
+		1: # EXTEND
+			pass
+		2: # LATCHED
+			pass
+		3: # PULL
+			pass
+		4: # PUSH
+			pass
+		_: # Default
+			print("Error")
+			pass
+	print(_hand.position)
+	print("-")
 
 # Aim Handling
 func process_aim() -> Vector2:
 	var mouse = get_viewport().get_mouse_position()
-	var aim = position - mouse
+	var aim = mouse - _player.position
 	return aim
 
 # Called when the node enters the scene tree for the first time.
@@ -33,7 +65,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	process_aim()
+	position_hand(process_aim())
 	process_movement(delta)
-	move_and_slide()
+	_player.move_and_slide()
 	
